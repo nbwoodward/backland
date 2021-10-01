b7ba0c7a926670347813380cae8dc44141c9a67b
Nick Woodward <nick.woodward@onxmaps.com>
2021-10-01 16:09:07 -0600

    test api

--- main.go
+++ main.go
@@ -9,6 +9,26 @@ import (
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
+// album represents data about a record album.
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data.
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
 func main() {
 	port := os.Getenv("PORT")
 
@@ -21,9 +41,7 @@ func main() {
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+	router.GET("/", getAlbums)
 
 	router.Run(":" + port)
 }
